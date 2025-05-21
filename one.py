43e9f20fe16cc617a8b2149f14f9570c9b180994
sandipa-pun <punsandipa04@gmail.com>
2025-05-21 13:48:05 +0545

    Update one.py

--- one.py
+++ one.py
@@ -1,8 +1,4 @@
 from selenium import webdriver
-<<<<<<< HEAD
-from selenium.webdriver.common.by import By
-=======
->>>>>>> 00d42342092d210e860dccbe87d780b55c20093a
 # from selenium.webdriver.chrome.service import service as ChromeService
 # from webdriver_manager.chrome import ChromeDriverManager
 
@@ -15,27 +11,6 @@ import time
 # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 # driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
 
-<<<<<<< HEAD
-browser = webdriver.Chrome()
-
-# #provide website
-
-# url = "https://merolagani.com"
-
-# driver.get(url)
-# driver.maximize_window()
-# title = driver.title
-# print(title)
-# time.sleep(3)
-
-# driver.quit()
-url = "https://demoqa.com/nestedframes"
-browser.get(url)
-iframe = browser.find_element(By.XPATH, "//body")
-browser.switch_to.frame(iframe)
-content = browser.find_element(By.XPATH, value = "//body").text
-print(content)
-=======
 driver = webdriver.Chrome()
 
 #provide website
@@ -49,4 +24,3 @@ print(title)
 time.sleep(3)
 
 driver.quit()
->>>>>>> 00d42342092d210e860dccbe87d780b55c20093a
