f883b9a05faebc84dd23718a2b2d42eabe20a846
Sandipa <sandi@sandeepa.com>
2025-05-22 20:18:45 +0545

    updated locator.py

--- locator.py
+++ locator.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -8,15 +7,26 @@ driver.get('https://www.thirdeyeadventure.com')
 driver.maximize_window()
 time.sleep(2)
 
-page_height = driver.execute_script("return document.body.scrollHeight")
-scroll_speed = 500
-scroll_iteration = int(page_height/scroll_speed)
+scroll_speed = 300
+max_scrolls = 50
 
-for _ in range(scroll_iteration):
+for _ in range(max_scrolls):
     try:
-        element = driver.find_element(By.CLASS_NAME, "title-content__wrapper")
-        driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element)
-        break 
+        element = driver.find_element(By.CLASS_NAME, "title-content__wrpper")
+
+       
+        is_visible = driver.execute_script("""
+            const rect = arguments[0].getBoundingClientRect();
+            return (
+                rect.top >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+            );
+        """, element)
+
+        if is_visible:
+            print("Element is now visible in viewport. Stopping scroll.")
+            break
+
     except:
         driver.execute_script(f"window.scrollBy(0, {scroll_speed});")
         time.sleep(1)
