8722743cdfab2fd81d6034263fd1e629231934a0
Sandipa <sandi@sandeepa.com>
2025-05-22 20:16:53 +0545

    updated test for merolagani

--- pytest/test_merolagani.py
+++ pytest/test_merolagani.py
@@ -7,7 +7,7 @@ import time
 
 @pytest.fixture(scope="module")
 
-
+# Initialize Browser
 def browser():
     driver= webdriver.Chrome()
     yield driver
@@ -25,8 +25,9 @@ def test_livetrading(browser):
     Market.click()
     time.sleep(2)
 
+    #Alert
     try:
-        browser.switch_to.alert.dismiss()
+        browser.switch_to.alert.accept()
         time.sleep(2)
     except:
         pass
@@ -35,27 +36,72 @@ def test_livetrading(browser):
     liveTrading.click()
     time.sleep(3)
 
+
+    #Scroll
     browser.execute_script("window.scrollBy(0,400);")
     time.sleep(3)
 
-    # frame_livetrading = browser.find_element(By.XPATH, '//*[@id="ctl00_ContentPlaceHolder1_LiveTrading"]')
-    # browser.switch_to.frame(frame_livetrading)
-    # browser.execute_script("window.scrollBy(0,500);")
-    # time.sleep(3)
+    trading = browser.find_element(By.XPATH, '//*[@id="ctl00_ContentPlaceHolder1_LiveTrading"]')
+    browser.execute_script("arguments[0].scrollTop = 1000", trading)
+    time.sleep(2)
 
+    # select
+    companies = browser.find_elements(By.XPATH, '//*[@id="ctl00_ContentPlaceHolder1_LiveTrading"]/table/tbody/tr/td[1]/a')
+    company = companies[0]
+    browser.execute_script("arguments[0].scrollIntoView({block: 'center'});", company)
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="ctl00_ContentPlaceHolder1_LiveTrading"]/table/tbody/tr[1]/td[1]/a')))
+    company.click()
+    time.sleep(2)
+
+    #Alert
     try:
-        frame = wait.until(EC.presence_of_element_located((By.ID, "ctl00_ContentPlaceHolder1_LiveTrading")))
-        browser.switch_to.frame(frame)
-    except Exception as e:
-        pytest.fail(f"Unable to switch to frame: {e}")
+        browser.switch_to.alert.accept()
+        time.sleep(2)
+    except:
+        pass
 
-    # Scroll inside the iframe
-    browser.execute_script("window.scrollBy(0, 500);")
+    # new tab
+
+    browser.switch_to.window(browser.window_handles[1])  # Move to the new tab
+    time.sleep(2)
+
+    # price history
+    page_height = browser.execute_script("return document.body.scrollHeight")
+    scroll_speed = 500
+    scroll_iteration = int(page_height/scroll_speed)
+
+    for _ in range(scroll_iteration):
+        try:
+            history = browser.find_element(By.ID, 'ctl00_ContentPlaceHolder1_CompanyDetail1_lnkHistoryTab')
+            browser.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", history)
+            time.sleep(1)
+            history.click()
+            break
+        except:
+            browser.execute_script("window.scrollBy(0, 500);")
+            time.sleep(1) 
+
+    time.sleep(2)
+
+
+    #Alert
+    try:
+        browser.switch_to.alert.accept()
+        time.sleep(2)
+    except:
+        pass
+
     
+    # date
+    date = browser.find_element(*(By.XPATH,"//input[@id='ctl00_ContentPlaceHolder1_CompanyDetail1_txtMarketDatePriceFilter']"))
+    # date = browser.find_element(By.XPATH, '//*[@id="ctl00_ContentPlaceHolder1_CompanyDetail1_txtMarketDatePriceFilter"]')
+    date.send_keys("05/21/2025")
+    time.sleep(2)
 
+    search = browser.find_element(By.XPATH, "//a[@id='ctl00_ContentPlaceHolder1_CompanyDetail1_lbtnSearchPriceHistory']")
+    search.click()
 
-
-
+    time.sleep(10)
 
 
 
